28b2d8e1af24242938303b8ea221e05df0b7aee8
swedutch <nbrasz@gmail.com>
2015-10-31 13:34:11 -0700

    Button prefab + sfx & music

---
+++ Assets/MusicController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+	
+	private static MusicController instance = null;
+	public static MusicController Instance {
+		get { return instance; }
+	}
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+}
